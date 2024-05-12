e1b9e906e5b86bc11a6b96038f1a52bf0e00bf50
rafomiya <omiyarafael@gmail.com>
2024-05-11 23:05:16 -0300

    structure image reader helper

--- ImageReader.c
+++ ImageReader.c
@@ -1,69 +1,91 @@
 #include "ImageReader.h"
 
-void read_mnist(double **arr, int amount)
+Dataset get_dataset(DatasetType type)
 {
-    FILE *file = fopen("./data/t10k-images.idx3-ubyte", "rb");
-    if (file)
+    Dataset dataset;
+    int trash;
+    int amount;
+
+    if (type == TRAINING)
     {
-        int magic_number = 0;
-        int amount_images = 0;
-        int n_rows = 0;
-        int n_cols = 0;
-
-        fread((char*) &magic_number, sizeof(magic_number), 1, file); 
-        magic_number = change_endian(magic_number);
-
-        fread((char*) &amount_images, sizeof(amount_images), 1, file);
-        amount_images = change_endian(amount_images);
-
-        fread((char*) &n_rows, sizeof(n_rows), 1, file);
-        n_rows = change_endian(n_rows);
-
-        fread((char*) &n_cols, sizeof(n_cols), 1, file);
-        n_cols = change_endian(n_cols);
-
-        for (int i = 0; i < min(amount_images, amount); ++i)
-        {
-            for (int r = 0; r < n_rows; ++r)
-            {
-                for (int c = 0; c < n_cols; ++c)
-                {
-                    unsigned char temp = 0;
-                    fread((char*) &temp, sizeof(temp), 1, file);
-                    arr[i][n_cols * r + c] = (double) temp;
-                }
-            }
-        }
+        dataset.images_file = fopen(PATH_TRAINING_IMAGES, READ_BINARY);
+        dataset.labels_file = fopen(PATH_TRAINING_LABELS, READ_BINARY);
+    }
+    else
+    {
+        dataset.images_file = fopen(PATH_TESTING_IMAGES, READ_BINARY);
+        dataset.labels_file = fopen(PATH_TESTING_LABELS, READ_BINARY);
     }
 
-    fclose(file);
+    // skipping the initial unnecessary bytes
+
+    // ignoring magic number from both files
+    fread((char *)&trash, sizeof(int), 1, dataset.images_file);
+    fread((char *)&trash, sizeof(int), 1, dataset.labels_file);
+
+    // reading amount of images
+    fread((char *)&amount, sizeof(int), 1, dataset.images_file);
+    fread((char *)&trash, sizeof(int), 1, dataset.labels_file); // it's same thing on both files
+    amount = change_endian(amount);
+
+    // ignoring dimension sizes from the images file
+    fread((char *)&trash, sizeof(int), 1, dataset.images_file); // 28
+    fread((char *)&trash, sizeof(int), 1, dataset.images_file); // 28
+
+    dataset.images = malloc(amount * sizeof(double *));
+    for (int i = 0; i < amount; ++i)
+    {
+        dataset.images[i] = malloc(789 * sizeof(double));
+    }
+
+    dataset.labels = malloc(amount * sizeof(char));
+    dataset.n = amount;
+
+    return dataset;
 }
 
-void print_image(double image[])
+void load_dataset(Dataset *dataset)
+{
+    unsigned char temp;
+    for (int i = 0; i < dataset->n; ++i)
+    {
+        for (int row = 0; row < 28; ++row)
+        {
+            for (int column = 0; column < 28; ++column)
+            {
+                fread((char *)&temp, sizeof(char), 1, dataset->images_file);
+                dataset->images[i][28 * row + column] = ((double)temp) / 256.0;
+            }
+        }
+
+        fread((char *)&temp, sizeof(char), 1, dataset->labels_file);
+        dataset->labels[i] = temp;
+    }
+}
+
+void destruct_dataset(Dataset dataset)
+{
+    fclose(dataset.images_file);
+    fclose(dataset.labels_file);
+    for (int i = 0; i < dataset.n; ++i)
+    {
+        free(dataset.images[i]);
+    }
+
+    free(dataset.images);
+    free(dataset.labels);
+}
+
+void print_image(double *image)
 {
     char *scale = " .:-=+*#%@";
     for (int i = 0; i < 28; ++i)
     {
         for (int j = 0; j < 28; ++j)
         {
-            printf("%c", scale[(int)(image[28 * i + j] * 10)]);
+            printf("%c ", scale[(int)(image[28 * i + j] * 10)]);
         }
         printf("\n");
     }
+    printf("\n\n\n");
 }
-
-int main()
-{
-    double arr[10000][28 * 28];
-    read_mnist(arr, 10000);
-
-    print_image(arr[0]);
-
-    return 0;
-}
-
-
-// oi rafa
-// precisa só terminar de debugar e ver se esse print image funciona
-// corrigir os warnings e tals
-// mas acho que ta certo =)
--- ImageReader.h
+++ ImageReader.h
@@ -1,2 +1,29 @@
-#include <stdio.h>
 #include "utils.h"
+#define READ_BINARY "rb"
+#define PATH_TRAINING_IMAGES "./data/train-images.idx3-ubyte"
+#define PATH_TRAINING_LABELS "./data/train-labels.idx1-ubyte"
+#define PATH_TESTING_IMAGES "./data/t10k-images.idx3-ubyte"
+#define PATH_TESTING_LABELS "./data/t10k-labels.idx1-ubyte"
+
+typedef enum
+{
+    TESTING,
+    TRAINING
+} DatasetType;
+
+// stores images from the specified dataset on an interval
+// [start, end)
+typedef struct
+{
+    FILE *images_file;
+    FILE *labels_file;
+    double **images;
+    char *labels;
+
+    int n;
+} Dataset;
+
+Dataset get_dataset(DatasetType type);
+void load_dataset(Dataset *dataset);
+void destruct_dataset(Dataset dataset);
+void print_image(double *image);
