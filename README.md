e813b33ebef2d37a87ee582f88552e2ba6eb21d8
rafomiya <omiyarafael@gmail.com>
2024-05-17 13:48:31 -0300

    add README.md and update main function with testing

---
+++ README.md
@@ -0,0 +1,10 @@
+# A Neural Network in C!
+
+This project is meant to test if I can actually program in C, and if I truly undestand the underlying concepts behind neural networks.
+
+To do this, I've used the [MNIST dataset](https://en.wikipedia.org/wiki/MNIST_database) of handwritten digits, inspired by the [3blue1brown](https://www.3blue1brown.com/topics/neural-networks) series on this subject.
+
+## TODO
+
+- Add the softmax derivative to the backpropagation function
+- Fix for more layers
--- main.c
+++ main.c
@@ -1,31 +1,41 @@
 #include "NeuralNetwork.h"
+#define AMOUNT_LAYERS 4
 
-void get_result(NeuralNetwork *nn, Dataset dataset, int sample);
+void get_result(NeuralNetwork *nn, Dataset *dataset, int sample);
+
+int max_index(double *arr, int n)
+{
+    int index = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        if (arr[i] > arr[index])
+        {
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+int test(NeuralNetwork *nn, Dataset *testing);
 
 int main()
 {
-    int amount_layers = 4;
-    int layer_sizes[4] = {28 * 28, 16, 16, 10};
-    Dataset training = get_dataset(TRAINING);
-    Dataset testing = get_dataset(TESTING);
+    int layer_sizes[AMOUNT_LAYERS] = {28 * 28, 36, 32, 10};
+    Dataset *training = get_dataset(TRAINING);
+    Dataset *testing = get_dataset(TESTING);
 
-    NeuralNetwork *nn = create_neural_network(amount_layers, layer_sizes);
+    NeuralNetwork *nn = create_neural_network(AMOUNT_LAYERS, layer_sizes);
 
-    load_dataset(&training);
-    load_dataset(&testing);
+    load_dataset(training);
+    load_dataset(testing);
 
-    // for (int i = 0; i < 10; ++i)
-    // {
-    //     printf("[%d]\n", testing.labels[i]);
-    //     print_image(testing.images[i]);
-    // }
-
-    stochastic_gradient_descent(nn, training, 0.01, 100);
-
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < 8; ++i)
     {
-        get_result(nn, testing, i);
+        stochastic_gradient_descent(nn, training, 0.01, 100);
     }
+
+    test(nn, testing);
 
     destruct_dataset(training);
     destruct_dataset(testing);
@@ -34,18 +44,54 @@ int main()
     return 0;
 }
 
-void get_result(NeuralNetwork *nn, Dataset dataset, int sample)
+void get_result(NeuralNetwork *nn, Dataset *dataset, int sample)
 {
-    compute(nn, dataset.images[sample]);
-    printf("[%d]\n", dataset.labels[sample]);
+    int d;
+
+    compute(nn, dataset->images[sample]);
+
+    printf("[%d]\n", dataset->labels[sample]);
+
     for (int i = 0; i < 10; ++i)
     {
-        printf("%d,\t", i);
+        printf("%d\t", i);
+        d = (int)(50 * nn->activations[AMOUNT_LAYERS - 1][i]);
+        for (int j = 0; j < d; ++j)
+        {
+            printf("$");
+        }
+        printf("\n");
     }
+
     printf("\n");
-    for (int i = 0; i < 10; ++i)
+}
+
+// returns the amount of correct samples
+int test(NeuralNetwork *nn, Dataset *testing)
+{
+    int result, last_layer_size = nn->layer_sizes[AMOUNT_LAYERS - 1];
+    int total = testing->n;
+    int amount_correct = 0;
+
+    for (int i = 0; i < testing->n; ++i)
     {
-        printf("%lf,\t", nn->activations[3][i]);
+        compute(nn, testing->images[i]);
+
+        result = max_index(nn->activations[AMOUNT_LAYERS - 1], last_layer_size);
+
+        if (testing->labels[i] == result)
+        {
+            amount_correct += 1;
+        }
+        else if (random() < 0.1) // 10% chance
+        {
+            print_image(testing->images[i]);
+            get_result(nn, testing, i);
+            printf("\n\n\n");
+        }
+
+        printf("Accuracy: %f%\n", 100.0 * (double)amount_correct / (double)(i + 1));
     }
-    printf("\n\n");
+
+    return amount_correct;
 }
